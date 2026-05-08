47d1b2e854928f85229e6bac98c410c1480fb9fc
buenoIago <iagocedup@gmail.com>
2026-05-08 16:20:00 -0300

    Adicionado cadastro de fornecedor

--- ControleMedicamentos.ConsoleApp/ModuloFornecedores/TelaFornecedor.cs
+++ ControleMedicamentos.ConsoleApp/ModuloFornecedores/TelaFornecedor.cs
@@ -19,6 +19,15 @@ public class TelaFornecedor : TelaBase<Fornecedor>, ITelaOpcoes, ITelaCrud
 
     protected override Fornecedor ObterDadosCadastrais()
     {
-        throw new NotImplementedException();
+        Console.Write("Digite o nome do fornecedor: ");
+        string nomeFornecedor = Console.ReadLine() ?? string.Empty;
+
+        Console.Write("Digite o Telefone do fornecedor: ");
+        string telefoneFornecedor = Console.ReadLine() ?? string.Empty;
+
+        Console.Write("Digite o CNPJ do fornecedor: ");
+        string cnpjFornecedor = Console.ReadLine() ?? string.Empty;
+
+        return new Fornecedor(nomeFornecedor, telefoneFornecedor, cnpjFornecedor);
     }
 }
