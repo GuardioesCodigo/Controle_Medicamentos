2451881546758b49ca51da149ab8498648dc2a09
buenoIago <iagocedup@gmail.com>
2026-05-08 16:28:52 -0300

    Adicionado vizualização de Fornecedores

--- ControleMedicamentos.ConsoleApp/ModuloFornecedores/TelaFornecedor.cs
+++ ControleMedicamentos.ConsoleApp/ModuloFornecedores/TelaFornecedor.cs
@@ -14,7 +14,30 @@ public class TelaFornecedor : TelaBase<Fornecedor>, ITelaOpcoes, ITelaCrud
 
     public override void VisualizarTodos(bool deveExibirCabecalho)
     {
+        if (deveExibirCabecalho)
+            ExibirCabecalho("Visualização de Fornecedores");
 
+        List<Fornecedor> fornecedors =  repositorio.SelecionarTodos();
+
+        if (fornecedors.Count == 0)
+        {
+            Notificador.ExibirMensagem("Nenhum fornecedor registrado");
+        }
+
+        foreach (Fornecedor f in fornecedors)
+        {
+            Console.WriteLine(
+            "{0, -7} | {1, -30} | {2, -15} | {3, -20}",
+            f.Id, f.Nome, f.Telefone, f.Cnpj
+        );
+        }
+
+        if (deveExibirCabecalho)
+        {
+            Console.WriteLine("---------------------------------");
+            Console.Write("Digite ENTER para continuar...");
+            Console.ReadLine();
+        }
     }
 
     protected override Fornecedor ObterDadosCadastrais()
