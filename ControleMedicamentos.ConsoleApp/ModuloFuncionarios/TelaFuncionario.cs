5a150dfac9761d6e4e9279af8a3039dec23943c7
buenoIago <iagocedup@gmail.com>
2026-05-11 15:44:13 -0300

    Adicionada validação de cpf duplicado

--- ControleMedicamentos.ConsoleApp/ModuloFuncionarios/TelaFuncionario.cs
+++ ControleMedicamentos.ConsoleApp/ModuloFuncionarios/TelaFuncionario.cs
@@ -47,7 +47,7 @@ public class TelaFuncionario : TelaBase<Funcionario>, ITelaOpcoes, ITelaCrud
         Console.Write("Digite o telefone do Funcionário: ");
         string telefoneFuncionario = Console.ReadLine() ?? string.Empty;
 
-        Console.Write("Digite o cpf do Funcionário: ");
+        Console.Write("Digite o CPF do Funcionário: ");
         string cpfFuncionario = Console.ReadLine() ?? string.Empty;
 
         return new Funcionario(nomeFuncionario, telefoneFuncionario, cpfFuncionario);
@@ -55,7 +55,19 @@ public class TelaFuncionario : TelaBase<Funcionario>, ITelaOpcoes, ITelaCrud
 
     protected override List<string> ValidarRegistroDuplicado(Funcionario novaEntidade, string? idIgnorado = null)
     {
-        return base.ValidarRegistroDuplicado(novaEntidade, idIgnorado);
+        List<string> erros = new List<string>();
+
+        List<Funcionario> funcionarios = new List<Funcionario>();
+
+        foreach (Funcionario f in funcionarios)
+        {
+            if (f.Id != idIgnorado && f.Cpf == novaEntidade.Cpf)
+            {
+                erros.Add($"Já existe um Funcionario com o CPF \"{novaEntidade.Cpf}\"");
+            }
+        }
+
+        return erros;
     }
 
 }
