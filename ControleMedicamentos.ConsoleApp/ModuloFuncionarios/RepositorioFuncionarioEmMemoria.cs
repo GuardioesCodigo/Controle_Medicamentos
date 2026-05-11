2ce21f8bf2e06e92d80db7dba32f34db0aaacc8e
buenoIago <iagocedup@gmail.com>
2026-05-11 16:01:08 -0300

    Removido Repositorios em Memoria

--- ControleMedicamentos.ConsoleApp/ModuloFuncionarios/RepositorioFuncionarioEmMemoria.cs
+++
@@ -1,7 +0,0 @@
-using System;
-using ControleMedicamentos.ConsoleApp.Compartilhado;
-using ControleMedicamentos.ConsoleApp.Compartilhado.Memoria;
-
-namespace ControleMedicamentos.ConsoleApp.ModuloFuncionarios;
-
-public class RepositorioFuncionarioEmMemoria : RepositorioBaseEmMemoria<Funcionario>, IRepositorio<Funcionario>;
